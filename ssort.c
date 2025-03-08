2f12ce5bbd3a9ce1698ecc8607199001fc655f29
Gabriel Bauer <gabeb1277@gmail.com>
2025-03-08 02:34:13 -0500

    Initial upload

---
+++ ssort.c
@@ -0,0 +1,160 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <math.h>
+
+#include "float_vec.h"
+#include "barrier.h"
+#include "utils.h"
+
+int compare(const void *a, const void *b) {
+	float fa = *(const float*)a;
+	float fb = *(const float*)b;
+	return (fa > fb) - (fa < fb);
+}
+
+void
+qsort_floats(floats* xs)
+{
+    // calls qsort to sort the array
+    // see "man 3 qsort" for details
+    //int n;
+    //for(n=0; xs->data[n]; n++);
+    qsort(xs->data, xs->size, sizeof(float), compare);
+    return;
+}
+
+floats*
+sample(float* data, long size, int P)
+{
+    // TODO: sample the input data, per the algorithm decription
+
+     floats* samp = make_floats(10);
+     for(int i=0; i<10&&*data; i++) samp->data[i] = *data++;
+     return samp;
+}
+
+void
+sort_worker(int pnum, float* data, long size, int P, floats* samps, long* sizes, barrier* bb)
+{
+    floats* xs = make_floats(10);
+    // TODO: select the floats to be sorted by this worker
+    for(int i=0; i<10&&*data; i++) xs->data[i] = *data++;
+
+    printf("%d: start %.04f, count %ld\n", pnum, samps->data[pnum], xs->size);
+
+    // TODO: some other stuff
+
+    qsort_floats(xs);
+
+    // TODO: probably more stuff
+
+    free_floats(xs);
+}
+
+void
+run_sort_workers(float* data, long size, int P, floats* samps, long* sizes, barrier* bb)
+{
+    pid_t kids[P];
+    (void) kids; // suppress unused warning
+
+    sort_worker(0, data, size, P, samps, sizes, bb);
+    // TODO: spawn P processes, each running sort_worker
+    for (int i=0; i<P; i++) {
+	    // TODO: Create processes...
+    }
+
+    for (int ii = 0; ii < P; ++ii) {
+        //int rv = waitpid(kids[ii], 0, 0);
+        //check_rv(rv);
+    }
+}
+
+void
+sample_sort(float* data, long size, int P, long* sizes, barrier* bb)
+{
+    floats* samps = sample(data, size, P);
+    run_sort_workers(samps->data, size, P, samps, sizes, bb);
+    free_floats(samps);
+}
+
+int
+main(int argc, char* argv[])
+{
+/*	int arr[8] = {6, 93, 45, 124, 5, 807, 30, 404};
+	int n;
+    for(n=0; arr[n]; n++);
+    qsort(arr, n, sizeof(int), compare);
+
+    for (int i = 0; arr[i]; i++) {
+	    printf("arr[%d] = %d\n", i, arr[i]);
+    }*/
+
+    alarm(120); // leave this here
+
+    if (argc != 3) {
+        printf("Usage:\n");
+        printf("\t%s P data.dat\n", argv[0]);
+        return 1;
+    }
+
+    const int P = atoi(argv[1]);
+    const char* fname = argv[2];
+
+    seed_rng();
+
+    int rv;
+    struct stat st;
+    rv = stat(fname, &st);
+    check_rv(rv);
+
+    const int fsize = st.st_size;
+    if (fsize < 8) {
+        printf("File too small.\n");
+        return 1;
+    }
+
+    int fd = open(fname, O_RDWR);
+    check_rv(fd);
+
+    void* file = malloc(1024); // TODO: load the file with mmap.
+    (void) file; // suppress unused warning.
+
+    long count = 0;
+    floats xs;
+    read(fd, &count, sizeof(long));
+    float* data = malloc(16*1024);
+    xs.data = data;
+
+    //printf("count = %ld\n", count);
+    int x, k=0;
+    float z;
+    while ((x = read(fd, &z, sizeof(float)))) {
+	    xs.data[k++]=z;
+    }
+    xs.size=k;
+    /*for(int i=0; xs.data[i]; i++)
+    	printf("%.4f\n", xs.data[i]);*/
+
+    long sizes_bytes = P * sizeof(long);
+    long* sizes = malloc(sizes_bytes); // TODO: This should be shared
+
+    barrier* bb = make_barrier(P);
+
+    sample_sort(xs.data, count, P, sizes, bb);
+    //floats_print(&xs);
+    free_barrier(bb);
+
+    free(sizes);
+    free(file);
+    free(data);
+    // TODO: munmap your mmaps
+
+    return 0;
+}
+
