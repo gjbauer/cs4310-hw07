36143fa1154444e68209258b170ac2d455b3e204
Gabriel Bauer <gabeb1277@gmail.com>
2025-04-28 21:31:51 -0400

    rm ssort.2.c -added algorithm descriptions in correct places...

--- ssort.c
+++ ssort.c
@@ -25,7 +25,6 @@ qsort_floats(floats* xs)
 {
     // TODO: call qsort to sort the array
     // see "man 3 qsort" for details
-    qsort(xs->data, xs->size, sizeof(float), compare);
 }
 
 floats*
@@ -33,9 +32,12 @@ sample(float* data, long size, int P)
 {
     // TODO: sample the input data, per the algorithm decription
     int samp[size];
-    for (int i=0; i<size; i++) samp[i] = rand() % (size + 1);
-    floats* xs = make_floats(size); // TODO: Implement random samples...
-    for (int i=0; i<size; i++) xs->data[i] = data[samp[i]];
+    /*
+     * Randomly select 3*(P-1) items from the array.
+     * Sort those items.
+     * Take the median of each group of three in the sorted array, producing an array (samples) of (P-1) items.
+     * Add 0 at the start and +inf at the end (or the min and max values of the type being sorted) of the samples array so it has (P+1) items numbered (0 to P).
+     */
     return xs;
 }
 
@@ -44,9 +46,9 @@ sort_worker(void* arg)
 {
     struct sort_args *sa = (struct sort_args*)arg;
     floats* xs = make_floats(3*sa->pnum);
-    int pos = 0;
     
-    for(int i=sa->pnum; i>0; i++) pos+=3*i;
+    // Each process uses quicksort to sort the local array.
+    
     // TODO: select the floats to be sorted by this worker
 
     printf("%d: start %.04f, count %ld\n", sa->pnum, sa->samps->data[pos], xs->size);
@@ -56,6 +58,13 @@ sort_worker(void* arg)
     qsort_floats(xs);
 
     // TODO: probably more stuff
+    
+    /* Each process calculates where to put its result array as follows:
+     *     start = sum(sizes[0 to p - 1]) # that’s sum(zero items) = 0 for p = 0
+     *     end = sum(sizes[0 to p]) # that’s sum(1 item) for p = 0
+     * Warning: Data race if you don’t synchronize here.
+     * Each process copies its sorted array to input[start..end]
+     */
 
     free_floats(xs);
     return 0;
@@ -68,18 +77,11 @@ run_sort_workers(float* data, long size, int P, floats* samps, long* sizes, barr
     (void) kids; // suppress unused warning
 
     // TODO: spawn P processes, each running sort_worker
-    for (int ii = 0; ii < P; ++ii) {
-        struct sort_args sa;
-        sa.pnum=ii;
-        sa.data=data;
-        sa.size=size;
-        sa.P=P;
-        sa.samps=samps;
-        sa.sizes=sizes;
-        sa.bb=bb;
-    	sizes[ii] = 3*ii;
-        pthread_create(&kids[ii], NULL, &sort_worker, (void *)&sa);
-    }
+    
+    /* Spawn P processes, numbered p in (0 to P-1).
+     * Each process builds a local array of items to be sorted by scanning the full input and taking items between samples[p] and samples[p+1].
+     * Write the number of items (n) taken to a shared array sizes at slot p.
+     */
 
     /*for (int ii = 0; ii < P; ++ii) {
         //int rv = waitpid(kids[ii], 0, 0);
@@ -135,9 +137,6 @@ main(int argc, char* argv[])
     for (int ii = 2; ii < count; ++ii) {
         printf("%.04f ", data[ii]);
     }
-
-    //printf("...", count);
-    //printf("...", data[0]);
 
     long sizes_bytes = P * sizeof(long);
     long* sizes = mmap(NULL, sizes_bytes, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, 0, 0);
